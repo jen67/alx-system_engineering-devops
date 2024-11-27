e243801b9ab38f99485fce7c38f92fdcd0098797
Gift Amachree <111980187+jen67@users.noreply.github.com>
2024-11-27 17:03:03 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import requests
+"""Module for task 1"""
+
 
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'python3:top_ten_posts:v1.0 (by /u/your_username)'}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code != 200:
-        print(None)
-        return
-    
-    data = response.json()
-    for post in data['data']['children']:
-        print(post['data']['title'])
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
